85f01ef93fb6093eb634a8d81ccf6877893b6043
Salem Ezz <sal@Salems-MacBook-Pro.local>
2018-06-04 15:44:06 -0400

    variables changed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,9 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('The Angery New Yorker');
+INSERT INTO burgers (burger_name) VALUES ('The Hazy Cali');
+INSERT INTO burgers (burger_name) VALUES ('The Tangy Carolina');
+
+
+-- INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
+-- INSERT INTO cats (name) VALUES ('Owens');
