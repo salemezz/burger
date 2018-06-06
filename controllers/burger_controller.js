6a9eb847a36054d43efe0e2cec4939eb52d439e4
Salem Ezz <sal@Salems-MacBook-Pro.local>
2018-06-06 10:17:51 -0400

    major revision

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -18,28 +18,31 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    "name", "devoured"
+    "burger_name", "devoured"
   ], [
-    req.body.name, req.body.devoured
+    req.body.name, 0
   ], function(result) {
     // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+    //res.json({ id: result.insertId });
+    res.redirect("/");
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.post("/api/burgers/:id", function(req, res) {  
   var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
+  //var notDevoured = req.body.devoured === false;
+  //console.log("devoured",notDevoured);
+  //console.log("condition", condition);
 
   burger.update({
-    devored: req.body.devoured
+    devoured: req.body.devoured === '0'
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
-      res.status(200).end();
+      //res.status(200).end();
+      res.redirect('/');
     }
   });
 });
